6fbbb7ca13d048b56f46c169995271a15a692c59
VrushabBayas <rushab.bayas@gmail.com>
2019-10-26 12:44:22 +0530

    Used custom hook for Galerry and Matrix component

--- home/src/App.js
+++ home/src/App.js
@@ -3,6 +3,8 @@ import Joke from "./Joke";
 import Stories from "./Stories";
 import Tasks from "./Tasks";
 import Gallery from "./Gallery";
+import Matrix from "./Matrix";
+
 function App() {
   const [userQuery, setUserQuery] = useState("");
   const [showGallery, setShowGallery] = useState(false);
@@ -36,6 +38,8 @@ function App() {
         <button onClick={searchQuery}>Search</button>
       </div>
       <hr />
+      <Matrix></Matrix>
+      <hr/>
       <div>
         {
           showGallery?<Gallery/>:null
